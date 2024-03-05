76be4fb578ae21fa0d41ae1c603945a952b141a3
Owen McCormick <owenmccr@gmail.com>
2024-03-05 00:26:51 -0500

    fix signup and 404 page

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,6 +1,6 @@
 export default function NotFound({ type = "User" }) {
   return (
-    <div className="flex h-screen w-screen flex-col items-center justify-center gap-2">
+    <div className="flex h-screen flex-col items-center justify-center gap-2">
       <h1 className="scroll-m-20 text-4xl font-bold tracking-tight lg:text-5xl">
         404 Not Found
       </h1>
